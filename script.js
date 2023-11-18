6b178082e6a7ed6f8ca43007d510f2c9123e0dd6
Kayk eli <kayksantana133@gmail.com>
2023-11-18 19:24:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,24 @@ const buttonStop = document.querySelector('.stop')
 const buttonSet = document.querySelector('.set')
 const buttonSoundOn = document.querySelector('.sound-on')
 const buttonSoundOFF = document.querySelector('.sound-off')
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+let minutes; 
+
+function countdown() { 
+  setTimeout(function() { 
+    let seconds = Number(secondsDisplay.textContent)
+
+    if (seconds <= 0) { 
+      seconds = 60
+    }
+
+    secondsDisplay.textContent = seconds - 1
+
+    countdown()
+
+  }, 1000 )
+}
 
 
 buttonPlay.addEventListener('click', () => {
@@ -11,6 +29,8 @@ buttonPlay.addEventListener('click', () => {
   buttonPause.classList.remove('hide')
   buttonStop.classList.remove('hide')
   buttonSet.classList.add('hide')
+
+  countdown()
 })
 
 buttonPause.addEventListener('click', () => {
@@ -34,4 +54,10 @@ buttonSoundOn.addEventListener('click', () => {
 buttonSoundOFF.addEventListener('click', () => { 
   buttonSoundOFF.classList.add('hide')
   buttonSoundOn.classList.remove('hide')
+})
+
+
+buttonSet.addEventListener('click', () => { 
+  minutes = prompt('Quantos minutos?')
+  minutesDisplay.textContent = minutes
 })
