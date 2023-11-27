1d64c282ac29bf6eba16ef71d01ccb10b6cd357f
Kayk eli <kayksantana133@gmail.com>
2023-11-27 18:21:33 -0300

    att

--- js/index.js
+++ js/index.js
@@ -1,57 +1,94 @@
-import Controls from "./controls.js";
-import Timer from "./timer.js";
+// DOM
+// Document referencia ao HTML
+// Primeiro declarar as variaveis que vou modificar ou atribuir um valor
+// Clean code (variaveis com nomes significativos)
 
+/* === MINHAS VARIAVEIS ====*/
 const buttonPlay = document.querySelector(".play");
 const buttonPause = document.querySelector(".pause");
 const buttonStop = document.querySelector(".stop");
 const buttonSet = document.querySelector(".set");
 const buttonSoundOn = document.querySelector(".sound-on");
-const buttonSoundOFF = document.querySelector(".sound-off");
+const buttonSoundOff = document.querySelector(".sound-off");
+
+/* Variveis responsaveis por mostrar o tempo na DOM*/
 const minutesDisplay = document.querySelector(".minutes");
 const secondsDisplay = document.querySelector(".seconds");
-let minutes = Number(minutesDisplay.textContent);
-let timeTimerOut;
 
-const controls = Controls(buttonPause, buttonPlay, buttonSet, buttonStop);
+let minutes;
 
-const timer = Timer({
-  minutesDisplay,
-  secondsDisplay,
-  timeTimerOut,
-  resetControls: controls.reset,
+function resetControls() { 
+  buttonPlay.classList.remove("hide");
+  buttonPause.classList.add("hide");
+  buttonSet.classList.remove("hide");
+  buttonStop.classList.add("hide");
+}
+
+function updateTimerDisplay(minutes, seconds) { 
+  minutesDisplay.innerHTML = String(minutes).padStart(2, "0");
+  secondsDisplay.innerHTML = String(seconds).padStart(2, "0");
+
+}
+
+function countdown() {
+  // Utilizando setTimeout para atrasar a execução de um trecho de código.
+  setTimeout(function () {
+    let seconds = Number(secondsDisplay.textContent); //Transformando String em Numero
+    let minutes = Number(minutesDisplay.textContent);
+
+    secondsDisplay.textContent = '00'
+
+    if (minutes <= 0) {
+      resetControls() 
+      return; //para de executar a função se minutes for 0, impede que seja minutes negativos
+    }
+
+    if (seconds <= 0) {
+      // Quando os segundos chegam a 0, reiniciar para 60
+
+      seconds = 5; //atualiza os segundos 
+
+      minutesDisplay.textContent = String(minutes - 1) //minutes a ser Diminuido,
+        .padStart(2, "0");
+    }
+
+    secondsDisplay.textContent = String(seconds - 1) //Seconds a ser Diminuido,
+      .padStart(2, "0"); // para adicionar um 0 quando tiver so 1 numeros,
+
+    countdown(); // colocar a propria função dentro dela para que execute novamente
+  }, 1000); // tempo para executar a funcao callback
+}
+
+// CallBacks
+buttonPlay.addEventListener("click", function () {
+  buttonPlay.classList.add("hide");
+  buttonPause.classList.remove("hide");
+  buttonSet.classList.add("hide");
+  buttonStop.classList.remove("hide");
+
+  countdown();
 });
 
-buttonPlay.addEventListener("click", () => {
-  controls.play();
-  timer.countdown();
+buttonPause.addEventListener("click", function () {
+  buttonPause.classList.add("hide");
+  buttonPlay.classList.remove("hide");
 });
 
-buttonPause.addEventListener("click", () => {
-  controls.pause();
-  clearTimeout(timeTimerOut);
+buttonStop.addEventListener("click", function () {
+  resetControls()
 });
 
-buttonStop.addEventListener("click", () => {
-  controls.reset();
-});
-
-buttonSoundOn.addEventListener("click", () => {
+buttonSoundOn.addEventListener("click", function () {
   buttonSoundOn.classList.add("hide");
-  buttonSoundOFF.classList.remove("hide");
+  buttonSoundOff.classList.remove("hide");
 });
 
-buttonSoundOFF.addEventListener("click", () => {
-  buttonSoundOFF.classList.add("hide");
+buttonSoundOff.addEventListener("click", function () {
+  buttonSoundOff.classList.add("hide");
   buttonSoundOn.classList.remove("hide");
 });
 
-buttonSet.addEventListener("click", () => {
-  let newMinutes = controls.getMinutes;
-  if (!newMinutes) {
-    timer.resetTimer();
-    return;
-  }
-  minutes = newMinutes;
-
-  timer.updateDisplay(minutes, 0);
+buttonSet.addEventListener("click", function () {
+  minutes = prompt("How many minutes?");
+  updateTimerDisplay(minutes, seconds)
 });
---
+++ note.md
@@ -0,0 +1,12 @@
+// Programação imperativa = Dar ordens passo a passo
+// Programação Declarativa = Dar ordens diretas
+
+// CALLBACK: uma callback é uma função que é passada como argumento para outra função e é executada após a conclusão de uma operação
+
+// REFATORAÇÃO: mudar um codigo para deixa-lo mais entendivel, SEM ALTERAR suas funcionalidades
+
+Sintaxe:
+setTimeout: (function() { }, tempo)
+
+Paramentros:
+ são parâmetros da função. Os parâmetros são variáveis que recebem valores quando a função é chamada. No contexto dessa função específica:
